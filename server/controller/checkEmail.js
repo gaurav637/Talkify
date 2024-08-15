291b3d3ecfb952504d4b744a41a12822383739cc
GauravNegi <negigaurav637@gmail.com>
2024-08-15 22:57:43 +0530

    complete Backend side basic code

---
+++ server/controller/checkEmail.js
@@ -0,0 +1,30 @@
+const UserModel = require("../models/UserModel")
+
+async function checkEmail(request,response){  // check email
+    try {
+        const { email } = request.body // get data
+
+        const checkEmail = await UserModel.findOne({email}).select("-password")
+
+        if(!checkEmail){
+            return response.status(400).json({
+                message : "user not exit",
+                error : true
+            })
+        }
+
+        return response.status(200).json({
+            message : "email verify",
+            success : true,
+            data : checkEmail
+        })
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true
+        })
+    }
+}
+
+module.exports = checkEmail
